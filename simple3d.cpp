cb06b1049387c9dff408e776a1dbc1fc0aa71eab
daneccf <danielecavalcantecf@gmail.com>
2021-02-18 22:16:15 -0300

    create quads

--- simple3d.cpp
+++ simple3d.cpp
@@ -5,6 +5,61 @@
 using namespace std;
 
 int cx = 0, cy = 0, cz = 0;
+int cn;
+
+struct Quads {
+	int x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4;
+	float r, g, b;
+	int state;
+	int total;
+}; 
+Quads Q[100];
+
+void addQuad() {
+	Q[0].state++;
+	if (Q[0].state > 4) {
+		Q[0].state = 1;
+	}
+	int st = Q[0].state;
+
+	if (st == 1) {
+		Q[0].total++;
+		cn = Q[0].total;
+	}
+	if (st == 1) {
+		Q[cn].x1 = cx;
+		Q[cn].y1 = cy;
+		Q[cn].z1 = cz;
+	}	
+	if (st == 1 || st == 2) {
+		Q[cn].x2 = cx;
+		Q[cn].y2 = cy;
+		Q[cn].z2 = cz;
+	}	
+	if (st == 1 || st == 2 || st == 3) {
+		Q[cn].x3 = cx;
+		Q[cn].y3 = cy;
+		Q[cn].z3 = cz;
+	}	
+	if (st == 1 || st == 2 || st == 3 || st == 4) {
+		Q[cn].x4 = cx;
+		Q[cn].y4 = cy;
+		Q[cn].z4 = cz;
+	}
+}
+
+void drawQuads() {
+	int i;
+	for (i = 1; i < Q[0].total + 1; i++) {
+		glBegin(GL_QUADS);
+		glColor3f(Q[i].r, Q[i].g, Q[i].b);
+		glVertex3f(Q[i].x1, Q[i].y1, Q[i].z1);
+		glVertex3f(Q[i].x2, Q[i].y2, Q[i].z2);
+		glVertex3f(Q[i].x3, Q[i].y3, Q[i].z3);
+		glVertex3f(Q[i].x4, Q[i].y4, Q[i].z4);
+		glEnd();
+	}
+}
 
 void theCube() {
 	glPushMatrix();
@@ -38,6 +93,7 @@ void display() {
 	glRotatef(40, 1, 1,0);
 
 	drawGrid();
+	drawQuads();
 	theCube();
 	glutSwapBuffers();
 }
@@ -58,6 +114,8 @@ void keyboard(unsigned char key, int x, int y) {
 	if (key == 'd') { cx += 1; } // right
 	if (key == 'q') { cy += 1; } // up
 	if (key == 'z') { cy -= 1; } // down
+
+	if (key == 32) { addQuad(); } //spacebar
 
 	glutPostRedisplay();
 }
